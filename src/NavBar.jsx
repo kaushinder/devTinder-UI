4188dbb5c0da2f3b68713c389dd425115f70ac90
kaushinder <kaushindersinghraghav@gmail.com>
2026-03-19 20:16:46 +0530

    Added Navbar and Footer

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <div className="navbar bg-base-300 shadow-sm">
+    <div className="navbar bg-neutral shadow-sm">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">DevTinder👨‍💻</a>
       </div>
